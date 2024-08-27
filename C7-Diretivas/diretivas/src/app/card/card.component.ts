9841e9b667613e784fbdf1f4bea4e7dfb07fc436
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-27 15:55:14 -0300

    finished course 7

--- C7-Diretivas/diretivas/src/app/card/card.component.ts
+++ C7-Diretivas/diretivas/src/app/card/card.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  produtos: string[] = [];
+  menuType: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.produtos = ['mouse', 'teclado', 'cabo', 'font'];
   }
 
+  ngOnInit(): void {}
+
+  adicionar() {
+    this.produtos.pop();
+  }
+
+  remover(index: number) {
+    this.produtos.splice(index, 1);
+  }
 }
