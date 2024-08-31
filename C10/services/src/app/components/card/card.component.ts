3697ecd6469d6b37c4d890f89f684a20ece04572
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-31 04:28:25 -0300

    added course 10

---
+++ C10/services/src/app/components/card/card.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+  pokemon?: PokemonData | any;
+  attributesTypes: string[] = ['Fire', 'Rock'];
+
+  constructor(private service: PokemonService) {}
+
+  ngOnInit(): void {
+    this.service.getPokemon('charizard').subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types,
+        };
+
+        console.log(res);
+        console.log(this.pokemon);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+}
---
+++ C10/services/src/app/models/pokemonData.ts
@@ -0,0 +1,8 @@
+export type PokemonData = {
+  types: any;
+  name: string;
+  id: number;
+  sprites: {
+    front_default: string;
+  };
+};
